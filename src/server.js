85bc8dacafe06aeaca0355eebe9f126522c77cf8
EwelaCode <e.kucharczyk2412@gmail.com>
2022-09-15 21:24:23 +0200

    simple poll

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(5000, () => console.log("Server Running..."));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+const pollData = require("./data.json");
+app.get("/poll", function (req, res) {
+  res.send(pollData);
+});
+
+app.post("/poll", function (req, res) {  
+    if (req.body) {
+      fs.writeFileSync("data.json", JSON.stringify(req.body));
+      res.send({
+        message: "Data Saved",
+      });
+    } else {
+      res.status(400).send({
+        message: "Error No Data",
+      });
+    }
+  });
